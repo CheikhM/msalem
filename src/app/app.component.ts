aa55995225da14ac6690ad242477546b74fdc745
mahfoudha091 <mohamedmahfoudha091@gmail.com>
2021-01-30 20:12:27 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,34 @@
-import { Component } from '@angular/core';
+import { CreateEmployeeComponent } from './employees/create-employee/create-employee.component';
+import { MatTableDataSource } from '@angular/material/table';
+import { Component, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatDialog } from '@angular/material/dialog';
+
+
+export interface PeriodicElement {
+  prenom: string;
+  fonction: string;
+  anneeExperience: number;
+  adress: string;
+  salarie: number;
+  dateNaissance: string;
+
+
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {prenom: 'Denis', fonction: 'Chef de projet', anneeExperience: 8, adress: 'Paris',salarie:4000,dateNaissance:'1988-10-20'},
+  {prenom: 'Denis', fonction: 'Chef de projet', anneeExperience: 8, adress: 'Paris',salarie:4000,dateNaissance:'1988-10-20'},
+  {prenom: 'Denis', fonction: 'Chef de projet', anneeExperience: 8, adress: 'Paris',salarie:4000,dateNaissance:'1988-10-20'},
+  {prenom: 'Denis', fonction: 'Chef de projet', anneeExperience: 8, adress: 'Paris',salarie:4000,dateNaissance:'1988-10-20'},
+  {prenom: 'Denis', fonction: 'Chef de projet', anneeExperience: 8, adress: 'Paris',salarie:4000,dateNaissance:'1988-10-20'},
+  {prenom: 'Denis', fonction: 'Chef de projet', anneeExperience: 8, adress: 'Paris',salarie:4000,dateNaissance:'1988-10-20'},
+  {prenom: 'Aenis', fonction: 'Chef de projet', anneeExperience: 8, adress: 'Paris',salarie:4000,dateNaissance:'1988-10-20'},
+  {prenom: 'Benis', fonction: 'Chef de projet', anneeExperience: 8, adress: 'Paris',salarie:4000,dateNaissance:'1988-10-20'},
+  
+ 
+];
 
 @Component({
   selector: 'app-root',
@@ -7,4 +37,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo';
+ displayedColumns: string[] = ['prenom', 'fonction', 'anneeExperience', 'adress','salarie','dateNaissance','actions'];
+  //dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+   dataSource:MatTableDataSource<PeriodicElement>;
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  searchKey: string;
+  constructor(public dialog: MatDialog) {}
+  ngOnInit() {
+    this.dataSource = new MatTableDataSource(ELEMENT_DATA);
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  onSearchClear() {
+    this.searchKey = "";
+    this.applyFilter();
+  }
+
+  applyFilter() {
+    this.dataSource.filter = this.searchKey.trim().toLowerCase();
+  }
+  openDialog() {
+    this.dialog.open(CreateEmployeeComponent);
+  }
+ 
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MaterialModule } from "./material/material.module";
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { CreateEmployeeComponent } from './employees/create-employee/create-employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateEmployeeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+   MaterialModule,
+   ReactiveFormsModule,
+  //AngularFireDatabaseModule,
+  //AngularFireModule.initializeApp(environment.firebaseConfig),
+  FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
